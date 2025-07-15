ba69833a73676059501b4c856ce62cac80f37a90
Thanoj <bsaithanoj@gmail.com>
2025-07-16 01:09:39 +0530

    added basic connection and home page

--- main.go
+++ main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
+var db *sql.DB
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func initDB() {
+
+	var err error
+
+	db, err = sql.Open("mysql", "root:demoapp@(127.0.0.1:3333)/mydb?parseTime=true")
+
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Error pinging the database:", err)
+	}
+}
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, HTMX with Go!")
-		http.ListenAndServe(":3000", nil)
-	})
+	initDB()
+	defer db.Close()
+
+	gRouter := mux.NewRouter()
+
+	gRouter.HandleFunc("/", handler)
+
+	http.ListenAndServe(":3000", gRouter)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	err := tmpl.ExecuteTemplate(w, "home.html", nil)
+
+	if err != nil {
+		log.Fatal("Error executing template:", err)
+	}
 }
